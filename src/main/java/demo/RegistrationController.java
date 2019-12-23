d7587076ff62762b3b2d0352ada931b5b30587d2
Administrator <Administrator@MUM00127CR1.NIITANDHERI.COM>
2019-12-23 16:41:43 +0530

    changes

---
+++ src/main/java/demo/RegistrationController.java
@@ -0,0 +1,40 @@
+package demo;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RegistrationController",urlPatterns = "/RegistrationController")
+public class RegistrationController extends HttpServlet 
+{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		User u=new User();
+		u.setUsername(req.getParameter("username"));
+		u.setEmail(req.getParameter("email"));
+		u.setPassword(req.getParameter("password"));
+		
+		try 
+		{
+			Connection con=DriverManager.getConnection("jdbc:mysql://34.67.32.70:3306/db","root","root");
+			PreparedStatement ps=con.prepareStatement("insert into users values(?,?,?)");
+			ps.setString(1, u.getUsername());
+			ps.setString(2, u.getEmail());
+			ps.setString(3, u.getPassword());
+			ps.executeUpdate();
+			resp.sendRedirect("login.jsp");
+		}
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+}
